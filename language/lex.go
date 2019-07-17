788550a1ecd4830cba87ec4ebe1ce1dd9004344d
oj <oj.lappi@gmail.com>
2019-07-17 04:21:32 +0300

    Working grammar with nonletter operators

--- language/lex.go
+++ language/lex.go
@@ -1,7 +1,9 @@
 package language
 
 import (
+	//"fmt"
 	"kugg/compilers/lex"
+	"strings"
 	"unicode"
 )
 
@@ -34,17 +36,15 @@ func lexRoot(lexer *lex.BaseLexer) lex.StateFn {
 top:
 	lexer.IgnoreSpaces()
 
-	letterFound := lexer.AcceptUnicodeRanges(unicode.Letter)
-	if letterFound {
-		return lexActionIdentifier
-	}
 	r := lexer.Next()
-	switch r {
-	case lex.EOF:
+	switch {
+	case unicode.IsLetter(r):
+		return lexActionIdentifier
+	case r == lex.EOF:
 		lexer.Emit(lex.TokenEOF)
 		return nil
 		//TODO:comments have only been implemented outside blocks...
-	case '#':
+	case r == '#':
 		lexer.AcceptUntil("\n")
 		//If we want to use comments for e.g. automatic
 		//generation of manuals/instructions
@@ -77,10 +77,15 @@ func lexActionIdentifier(lexer *lex.BaseLexer) lex.StateFn {
 		lexer.Emit(leftBrace)
 		return lexLHS
 	}
-
 	lexer.NotAllowedInContext(r, "action identifier")
 	return nil
 
+}
+
+func lexRightBrace(lexer *lex.BaseLexer) lex.StateFn {
+	lexer.Emit(rightBrace)
+	lexer.IgnoreSpaces()
+	return lexRoot
 }
 
 func lexLHS(lexer *lex.BaseLexer) lex.StateFn {
@@ -97,15 +102,8 @@ func lexLHS(lexer *lex.BaseLexer) lex.StateFn {
 	case r == '(':
 		lexer.Emit(leftParen)
 		return lexLHS
-	case r == ')':
-		//TODO: this is the only iffy part...
-		//But I believe I can now catch all VALID sentences, while allowing some false positives from the lexer
-		//We will catch those in the parser
-		lexer.Emit(rightParen)
-		return lexLHS
 	case r == '}':
-		lexer.Emit(rightBrace)
-		return lexActionIdentifier
+		return lexRightBrace
 	}
 
 	lexer.Unexpected(r, "identifier or left paren")
@@ -216,15 +214,42 @@ func lexRHS(lexer *lex.BaseLexer) lex.StateFn {
 		lexer.Emit(leftParen)
 		return lexLHS
 	}
-	lexer.Unexpected(r, "identifier or left paren")
-	return nil
+	//lexer.Unexpected(r, "identifier or left paren")
+	return lexOperator
 }
 
+//We've found a RHS operand, see what it is.
 func lexRHSOperand(lexer *lex.BaseLexer) lex.StateFn {
 	lexer.AcceptMatchOrRangeRun("_-", unicode.Letter, unicode.Digit)
 	lexer.Emit(name)
 
-	lexer.IgnoreSpaces()
+	return lexAfterRHS
+}
 
-	return lexLHS
+func lexAfterRHS(lexer *lex.BaseLexer) lex.StateFn {
+
+	lexer.AcceptSpaces()
+	newLine := strings.ContainsRune(lexer.Input[lexer.Start:lexer.Pos], '\n')
+	lexer.Ignore()
+
+	r := lexer.Next()
+	switch {
+	case unicode.IsLetter(r):
+		if newLine {
+			return lexLHSOperand
+		} else {
+			lexer.Unexpected(r, "operator or new line")
+			return nil
+		}
+	case r == '(':
+		lexer.Emit(leftParen)
+		return lexLHS
+	case r == ')':
+		lexer.Emit(rightParen)
+		return lexAfterRHS
+	case r == '}':
+		return lexRightBrace
+	}
+
+	return lexOperator
 }
